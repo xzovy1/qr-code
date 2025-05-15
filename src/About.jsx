18ab64aaccf737a70b3ee5b88ed5aea7fd2e56dc
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-15 14:55:34 -0600

    style: add white background to body. build: update references and publications

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,15 @@
 const About = () => {
     return (
+        <div className="body">
+
         <h2>Hi! I'm Brittany!</h2>
+        <h4>A Ph.D Candidate at the University of Alberta</h4>
+        
+        <div>Connect with me on <a href="https://www.linkedin.com/in/brittany-supina-241830268/">linkedin</a></div>
+            
+        
+
+        </div>
     )
 }
 
--- src/References.jsx
+++ src/References.jsx
@@ -1,36 +1,36 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import referenceList from "./referenceList";
 
 const References = () => {
-    const generateDummy = () => {
-        let arr = []
-        for (let i = 1; i < 30; i++){
-            arr.push({id: i,  name: `Dummy Reference ${i}`})
-        }
-        return arr;
-    }
-    const [references, setReferences] = useState(generateDummy);
+    // const generateDummy = () => {
+    //     let arr = []
+    //     for (let i = 1; i < 30; i++){
+    //         arr.push({id: i,  name: `Dummy Reference ${i}`})
+    //     }
+    //     return arr;
+    // }
+    const [references, setReferences] = useState(referenceList);
     const [index, setIndex] = useState(0);
     
     useEffect(()=>{
         function updateIndex(newIndex){
-            if(newIndex == 22)return setIndex(0)
+            if(newIndex == referenceList.length)return setIndex(0)
             setIndex(newIndex + 1)
         }
        let timer = setInterval(() => {
             updateIndex(index)
-        }, 2500)
+        }, 3000)
 
         return () => clearInterval(timer);
     }, [index])
-
     return(
-        <div className="references">
+        <div className="references body">
             <h2>References:</h2>
             
             <div className="reference-list">
 
-            <ul style={{ transform: `translateY(-${index * 3.5}%)`}}>
-                {references.map(ref => <li className="reference" key={ref.id}>{ref.name}</li>)}
+            <ul style={{ transform: `translateY(-${index * (3.5 * 3)}%)`}}>
+                {references.map(ref => <li className="reference" key={crypto.randomUUID()}>{ref}</li>)}
             </ul>
             </div>
         </div>
