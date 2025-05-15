a8608bc8e66fa240fcd1e1812a4c0e5f48001dc6
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-15 15:16:52 -0600

    style: clean up formatting

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,10 @@
 const About = () => {
     return (
         <div className="body">
-
-        <h2>Hi! I'm Brittany!</h2>
-        <h4>A Ph.D Candidate at the University of Alberta</h4>
-        
-        <div>Connect with me on <a href="https://www.linkedin.com/in/brittany-supina-241830268/">linkedin</a></div>
+            <h2>Hi! I'm Brittany!</h2>
+            <div>A Ph.D Candidate at the University of Alberta.</div>
             
-        
-
+            <div>Connect with me on <a href="https://www.linkedin.com/in/brittany-supina-241830268/">linkedin</a></div>
         </div>
     )
 }
