ed58047a4958638f7e0f2d29aed458515dd06310
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-16 04:42:32 -0600

    make image path absolute

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     return (
         <div className="body">
             <h2>Hi, I'm Brittany!</h2>
-            <img src="./src/assets/brittany.jpg" href='./src/assets/brittany.jpg' alt="" className="brittany-img"/>
+            <img src="/src/assets/brittany.jpg" href='./src/assets/brittany.jpg' alt="" className="brittany-img"/>
             <div>I'm a Ph.D. Candidate in the Dennis lab at the University of Alberta.</div>
             
             <div>Connect with me on <a href="https://www.linkedin.com/in/brittany-supina-241830268/">linkedin</a></div>
