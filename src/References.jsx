48ea3f56c6166bfafff0fd1b89c1e86a49c80e13
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-16 05:37:19 -0600

    make links for references

--- src/References.jsx
+++ src/References.jsx
@@ -33,33 +33,34 @@ const References = () => {
                 {/* {references.map(ref => <li className="reference" key={crypto.randomUUID()}>{ref}</li>)} */}
                 <li className="reference"> 
                     1. Ortega, L., and Rojas, C.M. (2021). Bacterial panicle blight and <i>Burkholderia glumae</i>: from
-pathogen biology to disease control. doi.org/10.1094/PHYTO-09-20-0401-RVW.
+pathogen biology to disease control. <a href="doi.org/10.1094/PHYTO-09-20-0401-RVW">doi.org/10.1094/PHYTO-09-20-0401-RVW</a>
                 </li>
 
                 <li className="reference">
                     2. Maeda, Y., Horita, M., Shinohara, H., Kiba, A., Ohnishi, K., Tsushima, S., and Hikichi, Y.
 (2007). Analysis of sources of oxolinic acid-resistant field strains of <i>Burkholderia glumae</i>
-based on rep-PCR analysis and nucleotide sequences of <i>gyrB</i> and <i>rpoD</i>.
-doi.org/10.1007/s10327-006-0313-9.
+based on rep-PCR analysis and nucleotide sequences of <i>gyrB</i> and <i>rpoD</i>. 
+<a href="doi.org/10.1007/s10327-006-0313-9"> doi.org/10.1007/s10327-006-0313-9</a>
                 </li>
 
                 <li className="reference">
                         3. Batuman, O., Britt-Ugartemendia, K., Kunwar, S., Yilmaz, S., Fessler, L., Redondo, A.,
 CHumachenko, K., Chakravarty, S., and Wade, T. (2024). The use and impact of antibiotics in
-plant agriculture: A review. doi.org/10.1094/PHYTO-10-23-0357-IA.
+plant agriculture: A review. <a href="doi.org/10.1094/PHYTO-10-23-0357-IA">doi.org/10.1094/PHYTO-10-23-0357-IA</a>
                 </li>
 
                 <li className="reference"> 
                     4. Weinberg, J.B., Alexander, B.D., Majure, J.M., Williams, L.W., Kim, J.Y., Vandamme, P., and
 LiPuma, J.J. (2007). <i>Burkholderia glumae</i> infection in an infant with chronic granulomatous
-disease. doi.org/10.1128/JCM.02058-06.
+disease. <a href="doi.org/10.1128/JCM.02058-06">doi.org/10.1128/JCM.02058-06</a>
                 </li>
 
                 <li className="reference">
                     5. Supina, B.S.I., McCutcheon, J.G., Peskett, S.R., Stothard, P., and Dennis, J.J. (2025). A
 flagella-dependent Burkholderia jumbo phage controls rice seedling rot and steers
-Burkholderia glumae toward reduced virulence in rice seedlings. doi.org/10.1128/mbio.02814-
-24.
+Burkholderia glumae toward reduced virulence in rice seedlings. <a href=">doi.org/10.1128/mbio.02814-
+24">doi.org/10.1128/mbio.02814-
+24</a>
 
                 </li>
 
@@ -67,7 +68,7 @@ Burkholderia glumae toward reduced virulence in rice seedlings. doi.org/10.1128/
 
                     6. Lauman, P., and Dennis, J.J. (2023). Synergistic interactions among <i>Burkholderia cepacia</i>
 complex-targeting phages reveal a novel therapeutic role for lysogenization-capable phages.
-doi.org/10.1128/spectrum.04430-22.
+<a href="doi.org/10.1128/spectrum.04430-22">doi.org/10.1128/spectrum.04430-22</a>
                 </li>
             </ul>
             </div>
